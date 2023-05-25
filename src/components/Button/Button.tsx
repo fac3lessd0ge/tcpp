5cd7cc5e60f21367995e6e05b7da7bdd415343c4
fac3lessd0ge <garmasharseniy@gmail.com>
2023-05-25 18:35:34 +0300

    :recycle: added onClick for button :)

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,19 +1,21 @@
-import React from "react";
+import React, { MouseEventHandler } from "react";
 import classes from "./Button.module.css";
 
 interface ButtonProps {
   color?: string;
   icon: React.ReactNode;
   left?: boolean;
+  onClick?: MouseEventHandler<HTMLButtonElement> | undefined;
 }
 
 export const Button = ({
   color,
   children,
   icon,
+  onClick
 }: React.PropsWithChildren<ButtonProps>) => {
   return (
-    <button type={"button"} style={{ color }}>
+    <button onClick={onClick} type={"button"} style={{ color }}>
       <div className={classes.iconContainer}>{icon}</div>
       {children ? (
         <div className={classes.textContainer}>{children}</div>
